68f946ae517395c4590e248afa4858f10ba8dbc1
Mejia <darkmejia643@gmail.com>
2023-08-15 11:45:00 -0600

    primeros pasos en api

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Employee;
+use Illuminate\Support\Facades\hash;
+use Illuminate\Support\Facades\Auth;
+
+class ApiController extends Controller
+{
+    //
+
+    function hola(Request $request){
+        $users= User::all();
+        $employe = Employee::all();
+        return response()->json($employe,200);
+
+    }
+
+    public function login(Request $request){
+
+        $response = ["status"=>0,"msg"=>""];
+        //dd( $request);
+        //$data = json_decode($request->getContent());//admin
+        $data = ['email'=>$request['email'],'password'=>$request['password']];
+        $user = User::where('email',$data['email'])->first();//admin@admin.com
+
+        if($user){
+
+            if(Hash::check($data['password'],$user->password)){//revisa los hashes segun el nombre de los arrays
+
+                //$token = $user->createToken("example");
+                Auth::attempt($data);
+                Auth::user();
+
+                $response["status"] = 1;
+                $response["msg"] = 'ini';//$token->plainTextToken;
+
+
+            }else{
+                $response["msg"] = "No existen un token registrado a este usuario.";
+            }
+
+        }else{
+            $response["msg"] = "Usuario no encontrado.";
+        }
+
+        return response()->json($response);
+    }
+
+
+    public function login1(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        if (Auth::attempt($credentials)) {
+            $user = Auth::user();
+            $token = $user->createToken('token-name')->plainTextToken;
+
+            return response()->json(['token' => $token]);
+        }
+
+        return response()->json(['message' => 'Unauthorized'], 401);
+    }
+    public function logon(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => 'required|email',
+            'password' => 'required',
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $user = User::where('email', $request->email)->first();
+            $token = $user->createToken('token-name')->plainTextToken;
+            return response()->json(['token' => $token], 200);
+        } else {
+            return response()->json(['message' => 'Invalid credentials'], 401);
+        }
+    }
+
+
+
+
+
+
+}
---
+++ routes/api.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Http\Controllers\ApiController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|1|QookLE8yykPc1qVsy4okxpO1UaFFewtPM7DldjX5"
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+
+return response()->json(["done",Auth::user()->name],200);
+
+});
+
+Route::get('/hola',[ApiController::class,'hola']);
+Route::post('/login',[ApiController::class,'login']);
+Route::post('/login1',[ApiController::class,'login1']);
+
+
+Route::post('/tokens/create', function (Request $request) {
+    $token = $request->user()->createToken($request->token_name);
+
+    return ['token' => $token->plainTextToken];
+});
